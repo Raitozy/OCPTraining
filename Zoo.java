b8c0068a24369868af01139c9a819c49a618df5e
Raitozy <juan.da.garcia@hotmail.com>
2021-05-18 00:44:36 +0200

    test javadoc

--- Zoo.java
+++ Zoo.java
@@ -1,5 +1,11 @@
 public class Zoo {
+    /**
+     * Main comment javadoc
+     * 
+     * @param args are the different arguments when using java Zoo...
+     */
     public static void main(String[] args) {
-        System.out.println("hi!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
