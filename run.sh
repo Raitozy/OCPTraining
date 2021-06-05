12aa8089a8f70446cb323963b32652fc647d7119
Raitozy <juan.da.garcia@hotmail.com>
2021-06-05 21:45:21 +0200

    run

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-javac packagea/ClassA.java packageb/ClassB.java
+# el -d da una carpeta para guardar los .class
+javac -d classes packagea/ClassA.java packageb/ClassB.java
 
-java packageb.ClassB
+#Is the same to write:
+# java -classpath
+# java --class-path
+java -cp classes packageb.ClassB
+
+#Crear un .jar
+# jar -cvf test.jar -C dir(Especificar un directorio para obtener los .java para crear el jar) .
+# jar --create --verbose --file test.jar .
